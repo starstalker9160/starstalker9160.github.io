c046a3c98fd64687418a1b0d12ce7cc04b42ef5d
starstalker9160 <abyazjschool@gmail.com>
2024-06-01 18:02:41 +0400

    added contact popup

---
+++ index.js
@@ -0,0 +1,23 @@
+function createPopup(id) {
+    let popupNode = document.querySelector(id);
+    let overlay = popupNode.querySelector(".overlay");
+    let closeBtn = popupNode.querySelector(".close-btn");
+    
+    function openPopup() {
+        popupNode.classList.add("active");
+    }
+    
+    function closePopup() {
+        popupNode.classList.remove("active");
+    }
+    
+    overlay.addEventListener("click", closePopup);
+    closeBtn.addEventListener("click", closePopup);
+    
+    return openPopup;
+}
+
+let popup = createPopup("#popup");
+document.querySelector("#open-popup").addEventListener("click", popup);
+
+console.log("ree");
