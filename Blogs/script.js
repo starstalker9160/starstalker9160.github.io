c5c5041647b5ea17e75d8421bbd57b4df0c7c04a
starstalker9160 <abyazjschool@gmail.com>
2024-07-03 12:09:23 +0400

    cool sh!t

--- Blogs/script.js
+++ Blogs/script.js
@@ -6,4 +6,11 @@ document.addEventListener('DOMContentLoaded', function() {
             window.location.href = url;
         });
     });
+    var clickableDivs2 = document.querySelectorAll('.carouselItem2');
+    clickableDivs2.forEach(function(div) {
+        div.addEventListener('click', function() {
+            var url = div.getAttribute('data-url');
+            window.location.href = url;
+        });
+    });
 });
