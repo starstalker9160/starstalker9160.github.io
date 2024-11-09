1532ca4c55e10130f5e1e99bada7acb4f15c1179
star.stalker9160 <abyazjschool@gmail.com>
2024-11-09 21:50:33 +0400

    major look overhaul

--- shelf/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var cards = document.querySelectorAll('.card');
-    cards.forEach(function(div) {
-        div.addEventListener('click', function() { if (div.getAttribute('data-url')){ window.location.href = div.getAttribute('data-url') }});
-    });
-});
