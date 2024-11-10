7f9f427cb0fcd81bd777f729d68c0c060ce1b00d
star.stalker9160 <abyazjschool@gmail.com>
2024-11-10 09:52:02 +0400

    writing nvim blog pt 1

---
+++ shelf/blogScript.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const codeElements = document.querySelectorAll('code[data-url]');
+    codeElements.forEach(function (codeElement) {
+        const fileUrl = codeElement.getAttribute('data-url');
+        fetch(fileUrl)
+            .then(response => {
+                if (!response.ok) { throw new Error('File not found'); }
+                return response.text();
+            })
+            .then(data => { codeElement.textContent = data; })
+            .catch(error => {
+                console.error('Error fetching the file:', error);
+                codeElement.textContent = 'Failed to load the file content.';
+            });
+    });
+
+
+    document.addEventListener('scroll', function() {
+        const docHeight = document.documentElement.scrollHeight;
+        const winHeight = window.innerHeight;
+        const scrollTop = window.scrollY;
+    
+        const scrollPercent = (scrollTop / (docHeight - winHeight)) * 100;
+    
+        const progressBar = document.getElementById('progress-bar');
+        progressBar.style.width = scrollPercent + '%';
+    });
+    
+});
